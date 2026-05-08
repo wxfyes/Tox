c62c38c2669d820012458fe612a75cd260bfcd8a
tianquege <wangn5635@gmail.com>
2026-05-08 20:51:15 +0800

    feat: optimize XHTTP performance and concurrency (clean)

--- core/xray/inbound.go
+++ core/xray/inbound.go
@@ -265,7 +265,14 @@ func buildV2ray(config *conf.Options, nodeInfo *panel.NodeInfo, inbound *coreCon
 			}
 			if inbound.StreamSetting.XHTTPSettings.ScMinPostsIntervalMs.From == 0 {
 				inbound.StreamSetting.XHTTPSettings.ScMinPostsIntervalMs = coreConf.Int32Range{
-					From: 30, To: 50,
+					From: 10, To: 30,
+				}
+			}
+			// 注入高并发配置，解决 YouTube 图片视频加载问题
+			if inbound.StreamSetting.XHTTPSettings.ReuseConfig == nil {
+				inbound.StreamSetting.XHTTPSettings.ReuseConfig = &coreConf.SplitHTTPReuseConfig{
+					MaxConcurrency: &coreConf.Int32Range{From: 64, To: 128},
+					MaxConnections: &coreConf.Int32Range{From: 4, To: 8},
 				}
 			}
 		}
